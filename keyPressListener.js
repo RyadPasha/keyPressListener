551ce93e55cc377ce6aa7982232eb2d4a5b86304
Mohamed Riyad <iMohamedRiyad1@gmail.com>
2021-02-09 20:00:31 +0200

    Fixing event handler in Internet Explorer

--- keyPressListener.js
+++ keyPressListener.js
@@ -1,16 +1,17 @@
-/* keyPressListener 1.7, 2021-02-09
+/* keyPressListener 1.8, 2021-02-09
  * https://github.com/ryadpasha/keyPressListener
  * Copyright (c) 2021 Ryad Pasha <@RyadPasha>
  * Licensed under the GNU General Public License v3.0 */
 var keyPressListener = {
     configObj: {
-      ignoreOtherInput: true,// If false and you set (Ctrl+A), when the user presses (Ctrl+A+C) it won't response.
-      defaultInput: 'key',   // Accepts: key, code.
-      splitter: '+',         // The splitter between each key.
-      version: 1.6
+      ignoreOtherInput: true, // If false and you passed for example (Ctrl+A), then if user presses more than the passed, for example (Ctrl+A+Y) or (Ctrl+A+Y+X ...etc.) it won't response.
+      defaultInput: 'key',    // Accepts: key, code.
+      splitter: '+',          // The splitter char between each key.
+      version: 1.8
     },
     config: function(configObject) {
         keyPressListener.configObj = Object.assign(keyPressListener.configObj, configObject);
+        return keyPressListener;
     },
     replaceArray: function(string, find, replace) {
       var replaceString = string.toUpperCase();
@@ -39,7 +40,7 @@ var keyPressListener = {
         document.addEventListener('keydown', function(e) {
           var e = e || window.event; // to deal with IE
           down[e.keyCode] = true;
-        })
+        });
         document.addEventListener('keyup', function(e) {
           var e = e || window.event; // to deal with IE
           var found = false;
@@ -47,15 +48,22 @@ var keyPressListener = {
             var index = nddCodes[i];
             if(down[index]) {
               found = true;
-            } else { found = false; break; }
+            } else
+              found = false;
+              break;
+            }
           }
-          if(!keyPressListener.configObj.ignoreOtherInput){
-            var entrd=0;for(var k in down){if(down[k])++entrd;}
+          if(!keyPressListener.configObj.ignoreOtherInput) {
+            var entrd=0;
+            for(var k in down) {
+              if(down[k]) ++entrd;
+            }
             var cond = (nddCodes.length == entrd);
           } else var cond = true;
           if(found && cond) callback();
           down[e.keyCode] = false;
-        })
+        });
       }
+      return keyPressListener;
     }
 };
